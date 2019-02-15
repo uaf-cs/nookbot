d069eb6d27ef9d427a38226937882a4a9d62f725
Chris Bailey <chris@chrisbailey.io>
2019-02-14 16:46:06 -0900

    Cleanup and reorganize

--- nookbot.rb
+++ nookbot.rb
@@ -1,6 +1,7 @@
 require 'discordrb'
 require 'net/https'
 require 'json'
+
 TOKEN = ENV['DISCORD_API_TOKEN']
 SERVER_ID = 478810581273673746
 CLASS_CATEGORY_ID = 478815208035581978
@@ -83,6 +84,36 @@ discord.command(:dropclass, description: 'Removes you from a class chat', usage:
 end
 
 
+discord.command(:classes, description: 'Lists classes', usage: 'classes') do |event|
+  message = "Currently available class channels:\n"
+  server.channels.select { |c| c.parent_id == CLASS_CATEGORY_ID }.each do |c|
+    message << "* #{ c.name }\n"
+  end
+  return message
+end
+
+
+discord.command(:source, description: 'Tells you where to find the source code', usage: 'source') do |event|
+  "https://github.com/FineTralfazz/NookBot"
+end
+
+
+discord.member_join do |event|
+  event.user.pm.send_embed do |embed|
+    embed.title = "Welcome to the UAF CS Discord! Here's some important info to get you started on the server."
+    embed.colour = 0x38a4f4
+
+    embed.add_field(name: "📛", value: "First things first, we need to know who you are! Message one of the admins(the people in yellow on the right when you're in the server) and tell them who you are.")
+    embed.add_field(name: "🏷️", value: "Next you need to set your name! If you're on a computer, right clicking yourself while in the server and selecting 'change nickname' will let you set your name for the server.")
+    embed.add_field(name: "📚", value: "Lastly, you can join specific class chats with the help of our resident NookBot. You can type the !classes command to see available classes and !joinclass (class-name) to join that class. Make sure you do all NookBot commands within the 'NookBot Den' channel.")
+    embed.add_field(name: "P.S.", value: "For all other rules ask an admin or see the server-rules channel.")
+  end
+end
+
+##
+# Fun stuff
+##
+
 discord.command(:roll, description: 'Performs a dice roll', usage: 'roll 20') do |event, dice|
   highest_number = Integer(dice) rescue nil
   if highest_number
@@ -103,6 +134,7 @@ discord.command(:cat, description: "Gives random cat", usage: 'cat') do |event|
   end
 end
 
+
 discord.command(:dog, description: "Gives random dog", usage: 'dog') do |event|
   url = 'https://dog.ceo/api/breeds/image/random'
   uri = URI(url)
@@ -113,6 +145,7 @@ discord.command(:dog, description: "Gives random dog", usage: 'dog') do |event|
   end
 end
 
+
 discord.command(:fact, description: "Gives random fact", usage: 'fact') do |event|
   url = 'http://randomuselessfact.appspot.com/random.json?language=en'
   uri = URI(url)
@@ -121,36 +154,10 @@ discord.command(:fact, description: "Gives random fact", usage: 'fact') do |even
   return random_fact
 end
 
-discord.command(:classes, description: 'Lists classes', usage: 'classes') do |event|
-  message = "Currently available class channels:\n"
-  server.channels.select { |c| c.parent_id == CLASS_CATEGORY_ID }.each do |c|
-    message << "* #{ c.name }\n"
-  end
-  return message
-end
-
-
-discord.command(:source, description: 'Tells you where to find the source code', usage: 'source') do |event|
-  "https://github.com/FineTralfazz/NookBot"
-end
-
 
 discord.command(:hotdog, description: "Gives a hotdog", usage: 'hotdog') do |event|
   event.channel.send_embed do |embed|
     embed.image = Discordrb::Webhooks::EmbedImage.new(url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Hotdog_-_Evan_Swigart.jpg/1200px-Hotdog_-_Evan_Swigart.jpg")
-  end
-end
-
-
-discord.member_join do |event|
-  event.user.pm.send_embed do |embed|
-    embed.title = "Welcome to the UAF CS Discord! Here's some important info to get you started on the server."
-    embed.colour = 0x38a4f4
-
-    embed.add_field(name: "📛", value: "First things first, we need to know who you are! Message one of the admins(the people in yellow on the right when you're in the server) and tell them who you are.")
-    embed.add_field(name: "🏷️", value: "Next you need to set your name! If you're on a computer, right clicking yourself while in the server and selecting 'change nickname' will let you set your name for the server.")
-    embed.add_field(name: "📚", value: "Lastly, you can join specific class chats with the help of our resident NookBot. You can type the !classes command to see available classes and !joinclass (class-name) to join that class. Make sure you do all NookBot commands within the 'NookBot Den' channel.")
-    embed.add_field(name: "P.S.", value: "For all other rules ask an admin or see the server-rules channel.")
   end
 end
 
