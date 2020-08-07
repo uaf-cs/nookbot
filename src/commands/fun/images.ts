8ffa20aea0bfd1c5e4d62e2a36bcc799c9ae8eb0
Izac Lorimer <izac@gordhoard.org>
2020-08-06 22:06:45 -0600

    Handle more errors

--- src/commands/fun/images.ts
+++ src/commands/fun/images.ts
@@ -3,25 +3,29 @@ import fetch from 'node-fetch'
 
 export const init = (bot: CommandClient): void => {
   bot.registerCommand('cat', async msg => {
-    const req = await fetch('https://aws.random.cat/meow')
-    if (req.ok) {
-      const randomKitty = await req.json()
-      await msg.channel.createMessage({
-        embed: {
-          image: {
-            url: randomKitty.file
+    try {
+      const req = await fetch('https://aws.random.cat/meow')
+      if (req.ok) {
+        const randomKitty = await req.json()
+        await msg.channel.createMessage({
+          embed: {
+            image: {
+              url: randomKitty.file
+            }
           }
-        }
-      })
-    } else {
-      await msg.channel.createMessage({
-        embed: {
-          description: "Hm, looks like I can't find any cats right now <:catto:535716923225276416>",
-          footer: {
-            text: `HTTP error ${req.status}`
+        })
+      } else {
+        await msg.channel.createMessage({
+          embed: {
+            description: "Hm, looks like I can't find any cats right now <:catto:535716923225276416>",
+            footer: {
+              text: `HTTP error ${req.status}`
+            }
           }
-        }
-      })
+        })
+      }
+    } catch (error) {
+      await msg.channel.createMessage(`Something went very wrong and I wasn't able to make the request.\n\`${(error as Error).message}\``)
     }
   }, {
     description: 'Gives a random cat',
@@ -29,25 +33,29 @@ export const init = (bot: CommandClient): void => {
   })
 
   bot.registerCommand('dog', async msg => {
-    const req = await fetch('https://dog.ceo/api/breeds/image/random')
-    if (req.ok) {
-      const randomDog = await req.json()
-      await msg.channel.createMessage({
-        embed: {
-          image: {
-            url: randomDog.message
+    try {
+      const req = await fetch('https://dog.ceo/api/breeds/image/random')
+      if (req.ok) {
+        const randomDog = await req.json()
+        await msg.channel.createMessage({
+          embed: {
+            image: {
+              url: randomDog.message
+            }
           }
-        }
-      })
-    } else {
-      await msg.channel.createMessage({
-        embed: {
-          description: "Hm, looks like I can't find any dogs right now :<",
-          footer: {
-            text: `HTTP error ${req.status}`
+        })
+      } else {
+        await msg.channel.createMessage({
+          embed: {
+            description: "Hm, looks like I can't find any dogs right now :<",
+            footer: {
+              text: `HTTP error ${req.status}`
+            }
           }
-        }
-      })
+        })
+      }
+    } catch (error) {
+      await msg.channel.createMessage(`Something went very wrong and I wasn't able to make the request.\n\`${(error as Error).message}\``)
     }
   }, {
     description: 'Gives a random dog',
