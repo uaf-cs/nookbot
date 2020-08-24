18a4d29680959a8c5f99aec0aa6f866d942e8f96
Izac Lorimer <izac@gordhoard.org>
2020-08-24 15:33:24 -0600

    Add join command to redirect to website

--- src/commands/classes/index.ts
+++ src/commands/classes/index.ts
@@ -4,6 +4,17 @@ import management from './management'
 
 export const init = (bot: CommandClient): void => {
   management.init(bot)
+
+  // Redirect to the website for joining classes
+  bot.registerCommand(
+    'joinclass',
+    'Class registration has been moved to https://nookbot.katlyn.dev/. Please use the website to join classes and gain access to their channels.',
+    {
+      aliases: ['join'],
+      hidden: true,
+      description: 'please don\'t use this command <:catto:535716923225276416>'
+    }
+  )
 }
 
 export default {
