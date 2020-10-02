4e0a225426aae511434a4e571fe235d44d38dfd3
Matheus Lemos <matheuslemosf@protonmail.com>
2020-10-02 10:49:29 -0300

    Added poll command
    Added command to start polls, given the poll title and its options.

--- src/commands/util/index.ts
+++ src/commands/util/index.ts
@@ -3,11 +3,13 @@ import { CommandClient } from 'eris'
 import evaluate from './evaluate'
 import kill from './kill'
 import status from './status'
+import poll from  './poll'
 
 export const init = (bot: CommandClient): void => {
   evaluate.init(bot)
   kill.init(bot)
   status.init(bot)
+  poll.init(bot)
 }
 
 export default {
---
+++ src/commands/util/poll.ts
@@ -0,0 +1,53 @@
+import { CommandClient, Message } from 'eris'
+
+interface Field {
+	name: string,
+	value: any,
+	inline: boolean
+}
+
+// Starts a new poll
+// Poll structure: !poll name of poll; option 1, emoji 1 ; option 2, emoji 2
+export const init = (bot: CommandClient): void => {
+	bot.registerCommand('poll', async (msg, args) => {
+
+		// Parse arguments and extract poll title,author and options
+		const opts:string[] = args.join(' ').split(';')
+		if (opts.length < 3) {
+			return 'Invalid poll\nCommand: !poll "Poll name"; [optionX, reactionX]';
+		}
+
+		const pollName = 'Poll: ' + opts.shift().trim(); 
+		const date = new Date()
+		const author = 'Created by ' + msg.author + ' in ' + date;
+
+		// Create 'fields' array with poll options and reactions
+		const fields = []
+		for (let i in opts) {
+			let res = <Field>{}
+			const parsed = opts[i].split(',') 
+			const key = parsed[0].trim();
+			const value = parsed[1].trim()
+			res = {
+				name: key,
+				value: value,
+				inline: false
+			};
+			fields.push(res);
+		}
+
+		msg.channel.createMessage({
+			embed: {
+				title: pollName,
+				fields: fields,
+				footer: {
+					text: author
+				}
+			}
+		});
+	})
+}
+
+export default {
+	init
+}
