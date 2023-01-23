7e7a5e73667e0bfb302e1b33831a8cc28e5928be
Katlyn Lorimer <katlyn@katlyn.dev>
2023-01-23 10:52:02 -0900

    fix: add required passport logout callback

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -9,13 +9,21 @@ router.use('/discord', discord)
 router.use('/google', google)
 
 router.get('/login', (req, res) => {
-  req.logout()
-  res.redirect('google')
+  req.logout(err => {
+    if (err !== null) {
+      throw err
+    }
+    res.redirect('google')
+  })
 })
 
 router.get('/logout', (req, res) => {
-  req.logout()
-  res.redirect('/')
+  req.logout(err => {
+    if (err !== null) {
+      throw err
+    }
+    res.redirect('/')
+  })
 })
 
 export default router
